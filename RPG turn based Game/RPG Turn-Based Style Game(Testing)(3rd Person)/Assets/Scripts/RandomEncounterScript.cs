2882579333655da7a8687c9c6e2af5c716d017c8
Adam MILBOURNE <milbourneadam@yahoo.com>
2021-11-09 09:45:15 +0000

    Nothing Major

--- RPG turn based Game/RPG Turn-Based Style Game(Testing)(3rd Person)/Assets/Scripts/RandomEncounterScript.cs
+++ RPG turn based Game/RPG Turn-Based Style Game(Testing)(3rd Person)/Assets/Scripts/RandomEncounterScript.cs
@@ -4,40 +4,55 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class RandomEncounterScript : MonoBehaviour
 {
     [SerializeField] private GameObject encounterConfirm;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool enemyEncountered;
+    [SerializeField]  public float period = 2.0f;
+    [SerializeField] public float time = 0.0f;
+
+    public void encounterEnemy()
     {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
+        if (enemyEncountered == true)
+        {
+            Debug.Log("enemy encountered");
+            StartCoroutine(encounterConfirmed());
+        }
     }
 
     
-
+    
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player")
+        time += Time.deltaTime;
+
+        if (time >= period)
         {
-            if (Random.Range(1, 101) == 10)
+            time = time - period;
+            if (other.tag == "Player")
             {
-                Debug.Log("player has entered");
-                StartCoroutine(encounterConfirmed());
+
+                int Encounter = Random.Range(1, 10);
+                if (Encounter == 1)
+                {
+                    enemyEncountered = true;
+                    encounterEnemy();
+                }
             }
         }
     }
 
     public IEnumerator encounterConfirmed()
     {
+       
         encounterConfirm.SetActive(true);
         yield return new WaitForSeconds(2);
         encounterConfirm.SetActive(false);
     }
 
+   
+    
+    
 }
